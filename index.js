46c4e2d21c7b02cae7d39284365d31a0acfa0db3
YShameko <yshameko@gmail.com>
2025-07-21 19:36:46 +0300

    hw12.2

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const buttonsSet = document.querySelector('.buttons-set');
 if (buttonsSet) {
     buttonsSet.addEventListener('click', event => {
     if (event.target.tagName === "BUTTON") {
-        const MessageToShow = 'Клікнуто на кнопці: ' + event.target.textContent;
-        alert(MessageToShow);
+        const messageToShow = 'Клікнуто на кнопці: ' + event.target.textContent;
+        alert(messageToShow);
     }
     });
 } 
